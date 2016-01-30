acdb3a83fec65c246b80fd57c97c725737a542a6
mcnedward <edwardmcn64@gmail.com>
2016-01-30 15:02:51 +0000

    Add Slide line for Crosshair script

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -1,77 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
-    [RequireComponent(typeof (LineRenderer))]
     public class Crosshair : MonoBehaviour
     {
         private FirstPersonController _controller;
         private Terrain _terrain;
-        //private LineRenderer _lineRenderer;
+
+        // Crosshair stuff
+        private float _crosshairWidth = 10f;
+        private float _crosshairHeight = 10f;
+
+        private bool _showSlide;
+        private Vector3 _slidePosition;
+        private float _slideDistance;
+        // Determines if the mouse button is still being pressed down
+        private bool _buttonDown;
+
+        [SerializeField] private float _slideRange = 100f;
+        [SerializeField] private LineRenderer _lineRenderer;
+        [SerializeField] private GameObject _sphere;
 
         // Use this for initialization
         void Start()
         {
             _controller = FindObjectOfType<FirstPersonController>();
             _terrain = FindObjectOfType<Terrain>();
-            //_lineRenderer = GetComponent<LineRenderer>();
-            //_lineRenderer.enabled = false;
+            _lineRenderer = GetComponent<LineRenderer>();
+            _lineRenderer.enabled = false;
         }
 
         // Update is called once per frame
         void Update()
         {
-            CastRayToTerrain();
+            if (_showSlide)
+            {
+                print("Mouse down at " + _slidePosition);
+                Slide();
+            }
+            else
+            {
+                _lineRenderer.enabled = false;
+            }
         }
 
         void FixedUpdate()
         {
+            CastRayToTerrain();
         }
+
+        private int x = 0;
 
         private void CastRayToTerrain()
         {
             RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (_terrain.GetComponent<Collider>().Raycast(ray, out hit, 100))
+            var worldCenter = new Vector3(Screen.width / 2, Screen.height / 2);
+            var ray = Camera.main.ScreenPointToRay(worldCenter);
+
+            if (Input.GetButtonDown("Fire1") || _buttonDown)
             {
-                print("WORLD POINT: " + hit.point);
-                if (Input.GetMouseButtonDown(0))
+                _buttonDown = true;
+                if (Physics.Raycast(ray, out hit, _slideRange))
                 {
+                    _slidePosition = hit.point;
+                    _slideDistance = hit.distance;
+                    // Check if the distance is within sliding range
+                    _showSlide = hit.distance != 0;
                 }
-                if (Input.GetMouseButtonUp(0))
+                else
+                    _showSlide = false;
+            }
+            if (Input.GetButtonUp("Fire1") || !_buttonDown)
+            {
+                _buttonDown = false;
+                _showSlide = false;
+                if (Physics.Raycast(ray, out hit, _slideRange))
                 {
                     if (hit.distance != 0)
-                        _controller.transform.position = hit.point;
+                    {
+                        // Slide is within range, so Slide!
+                        //_controller.transform.position = hit.point;
+                    }
                 }
             }
         }
 
         private void Slide()
         {
-            // Find the position that the mouse is hitting
-            var mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var ray = Camera.main.ScreenPointToRay(mouseWorldPosition);
-            RaycastHit hit;
-            Physics.Raycast(ray, out hit, 1000);
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                //_lineRenderer.enabled = true;
-            }
-            if (Input.GetMouseButtonUp(0))
-            {
-                //_lineRenderer.enabled = false;
-                if (hit.distance != 0)
-                    _controller.transform.position = hit.point;
-            }
+            var y = _slidePosition.y - _controller.transform.position.y;
+            var linePoint = new Vector3(0f, y, _slideDistance);
+            _lineRenderer.SetPosition(1, linePoint);
+            _lineRenderer.enabled = true;
         }
 
         void OnGUI()
         {
-            GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 10, 10), "");
+            GUI.Box(
+                new Rect(Screen.width / 2 - (_crosshairWidth / 2), Screen.height / 2 - (_crosshairHeight / 2),
+                    _crosshairWidth, _crosshairHeight), "");
         }
     }
 }
